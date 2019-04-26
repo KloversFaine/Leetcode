4cc6568e0e774a42cde228d5ade6a659f5241eed
KloversFaine <ltl0124@163.com>
2019-04-26 14:41:03 +0800

    Update Leetcode_question.sql

--- Leetcode_question.sql
+++ Leetcode_question.sql
@@ -7,3 +7,16 @@ ON A.PersonId = B.PersonId;
 #sql query 176
 select MAX(Salary) AS SecondHighestSalary from Employee
 where Salary < (select MAX(Salary) from Employee);
+
+#sql query 177
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select distinct Salary from Employee 
+      order by Salary DESC
+      LIMIT M,1
+  );
+END
