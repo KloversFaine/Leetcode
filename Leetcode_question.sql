d71745ea3525123fab4c7c55233e78ed63e7dc2c
KloversFaine <ltl0124@163.com>
2019-05-08 17:20:12 +0800

    Update Leetcode_question.sql

--- Leetcode_question.sql
+++ Leetcode_question.sql
@@ -120,6 +120,14 @@ where DATEDIFF(w1.RecordDate,w2.RecordDate) = 1 and w1.Temperature > w2.Temperat
 select t.Request_at Day, ROUND(sum((case when t.Status like 'cancelled%' then 1 else 0 end))/count(*),2) AS 'Cancellation Rate'
 from Trips t
 inner join Users u on u.Users_Id = t.Client_Id and u.Banned = 'No'
- 
-
 where t.Request_at between '2013-10-01' and '2013-10-03' group by t.Request_at
+
+#sql query 595
+select name, population, area
+from World 
+where area > 3000000 OR population > 25000000
+
+#sql query 596
+select class from courses 
+group by class
+having count(distinct student) >= 5
