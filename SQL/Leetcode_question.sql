56e21616fbccd93c08605b4341d9986918233ac1
KloversFaine <ltl0124@163.com>
2019-05-10 15:49:40 +0800

    Update Leetcode_question.sql

--- SQL/Leetcode_question.sql
+++ SQL/Leetcode_question.sql
@@ -152,3 +152,7 @@ select *
 from cinema
 where description != 'boring' and  MOD(id, 2) = 1 
 order by rating desc;
+
+#sql query 626
+select if(id < (select count(*) from seat), if(id mod 2=0, id-1, id+1), if(id mod 2=0, id-1, id))  as id, student
+from seat order by id asc
